ecff90622bd15b5720a1b52107cb5386b331e996
Elangoavn M <69704702+gitelangovan@users.noreply.github.com>
2024-11-26 14:47:26 +0530

    Create objAndArryConv.js
    Converting object to array and array to object

---
+++ objAndArryConv.js
@@ -0,0 +1,29 @@
+      //Object to array using Object Static Method
+			const empDetails = {name: 'elangovan', age: 27, address: 'chennai'};
+			
+			const objKeys = Object.keys(empDetails);
+			//console.log(objKeys);
+
+			const objKValues = Object.values(empDetails);
+			//console.log(objKValues);
+
+			const objAll = Object.entries(empDetails);
+			console.log(objAll);
+	
+			var allArr = [];
+			for(let arr in empDetails){
+				allArr.push(arr,empDetails[arr]);
+			}
+			console.log(allArr);
+
+
+			// Array into Objects
+			const empDetails1 = ['name', 'elangovan'];
+		
+			var convObj = empDetails1.reduce((acc, crr, index) => {
+				if(index % 2 ===0){
+					acc[crr] = empDetails1[index+1];
+				}
+				return acc;
+			}, {});
+			console.log(convObj);
